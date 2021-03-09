64230d89e8ea34069eb1bf28486a463860d558b2
Akbar Rizqi Luhur Pambudi <akbar.pambudi@amarbank.co.id>
2021-03-09 17:51:35 +0700

    feat: create domain repository for `category` entity

---
+++ internal/library/domain/category/repository.go
@@ -0,0 +1,11 @@
+package category
+
+import "context"
+
+type (
+	Repository interface {
+		Create(ctx context.Context, entity Category) error
+		Update(ctx context.Context, id string, entity Category) error
+		Load(ctx context.Context, id string) (entity Category, err error)
+	}
+)
