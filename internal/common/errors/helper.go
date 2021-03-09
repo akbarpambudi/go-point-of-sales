c2ac4877c4023a8a9f893d68f65aa1759d4c3983
Akbar Rizqi Luhur Pambudi <akbar.pambudi@amarbank.co.id>
2021-03-10 01:10:27 +0700

    feat: create helper function for constructing POSError on errors pkg

---
+++ internal/common/errors/helper.go
@@ -0,0 +1,17 @@
+package errors
+
+func NewAuthorizationError(messageKey string, message string) error {
+	return NewPOSError(ErrorTypeAuthorizationError, messageKey, message)
+}
+
+func NewAuthenticationError(messageKey string, message string) error {
+	return NewPOSError(ErrorTypeAuthenticationError, messageKey, message)
+}
+
+func NewInternalError(messageKey string, message string) error {
+	return NewPOSError(ErrorTypeInternalError, messageKey, message)
+}
+
+func NewIllegalInputError(messageKey string, message string) error {
+	return NewPOSError(ErrorTypeIllegalInputError, messageKey, message)
+}
