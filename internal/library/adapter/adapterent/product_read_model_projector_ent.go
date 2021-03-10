5fa5518739f2420b6a2e9cba07c7d0e024a6f44d
Akbar Rizqi Luhur Pambudi <akbar.pambudi@amarbank.co.id>
2021-03-10 10:29:37 +0700

    feat: add variant on product read model

--- internal/library/adapter/adapterent/product_read_model_projector_ent.go
+++ internal/library/adapter/adapterent/product_read_model_projector_ent.go
@@ -11,9 +11,21 @@ func (p ProductRepository) LoadById(ctx context.Context, id string) (query.Produ
 		return query.Product{}, err
 	}
 
+	var variants []query.Variant
+
+	for _, v := range domainEntity.Variants() {
+		variants = append(variants, query.Variant{
+			ID:    v.ID(),
+			Code:  v.Code(),
+			Name:  v.Name(),
+			Price: v.Price(),
+		})
+	}
+
 	return query.Product{
 		ID:       domainEntity.ID(),
 		Name:     domainEntity.Name(),
 		Category: domainEntity.CategoryRef(),
+		Variants: variants,
 	}, nil
 }
--- internal/library/app/query/types.go
+++ internal/library/app/query/types.go
@@ -4,4 +4,12 @@ type Product struct {
 	ID       string
 	Name     string
 	Category string
+	Variants []Variant
+}
+
+type Variant struct {
+	ID    string
+	Code  string
+	Name  string
+	Price float64
 }
