6f38ccf3fc6b27e1bdd0c4d5c3914552fef113a1
Akbar Rizqi Luhur Pambudi <akbar.pambudi@amarbank.co.id>
2021-03-11 05:37:57 +0700

    refactor: make category repository to return and accept pointer of category entity

--- internal/library/domain/category/repository.go
+++ internal/library/domain/category/repository.go
@@ -4,8 +4,8 @@ import "context"
 
 type (
 	Repository interface {
-		Create(ctx context.Context, entity Category) error
-		Update(ctx context.Context, id string, entity Category) error
-		Load(ctx context.Context, id string) (entity Category, err error)
+		Create(ctx context.Context, entity *Category) error
+		Update(ctx context.Context, id string, entity *Category) error
+		Load(ctx context.Context, id string) (entity *Category, err error)
 	}
 )
