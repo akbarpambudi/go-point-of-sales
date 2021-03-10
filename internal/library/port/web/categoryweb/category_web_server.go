751c6b360f929a2aa0b5fbeedf0b49d33b83afc9
Akbar Rizqi Luhur Pambudi <akbar.pambudi@amarbank.co.id>
2021-03-11 06:30:34 +0700

    feat: generate web api handler for category base on the api spec

---
+++ internal/library/port/web/categoryweb/category_web_server.go
@@ -0,0 +1,37 @@
+package categoryweb
+
+import (
+	"github.com/akbarpambudi/go-point-of-sales/internal/common/httphelper"
+	"github.com/akbarpambudi/go-point-of-sales/internal/common/ptrval"
+	"github.com/akbarpambudi/go-point-of-sales/internal/library/app"
+	"github.com/akbarpambudi/go-point-of-sales/internal/library/app/command"
+	"github.com/labstack/echo/v4"
+)
+
+type Server struct {
+	application app.Application
+}
+
+func NewServer(application app.Application) *Server {
+	return &Server{application: application}
+}
+
+func (s Server) CreateCategory(ctx echo.Context) error {
+
+	var reqBody CreateCategoryJSONRequestBody
+
+	if err := ctx.Bind(&reqBody); err != nil {
+		return httphelper.WrapError(err)
+	}
+
+	err := s.application.Commands.CreateCategory.Handle(ctx.Request().Context(), command.CreateCategory{
+		ID:   ptrval.StringVal(reqBody.Id),
+		Name: ptrval.StringVal(reqBody.Name),
+	})
+
+	if err != nil {
+		return httphelper.WrapError(err)
+	}
+
+	return nil
+}
